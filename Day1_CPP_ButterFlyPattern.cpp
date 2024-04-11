4b250fe8e4122f76fe79741a731fddc3c80571c3
swetsanjeev <74481866+swetsanjeev@users.noreply.github.com>
2024-04-12 03:20:04 +0530

    Add files via upload

---
+++ Day1_CPP_ButterFlyPattern.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int row;
+	cin >> row;
+	bool reverse = false;
+	int i = 1;
+	while (i > 0)
+	{
+		int j;
+		string star(i, '*');
+		cout << star;
+		j = 0;
+		while (j < (2 * (row - i)))
+		{
+			cout << " ";
+			j++;
+		}
+		cout << star;
+		cout << endl;
+
+		reverse ? i-- : i++;
+		if (i > row)
+		{
+			reverse = true;
+			i--;
+		}
+
+	}
+	return 0;
+}
